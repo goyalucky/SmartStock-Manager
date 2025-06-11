cca466e7d4e1785dcd12798429849a3e4cc3da5a
goyallucky <luckygoyal221982@gmail.com>
2025-06-11 20:08:26 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react';
+
+function App() {
+  
+
+  return (
+    <div className='text-3xl font-bold underline'>
+      React App
+    </div>
+  )
+}
+
+export default App
