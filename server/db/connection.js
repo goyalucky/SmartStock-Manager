e659f8a88305f1b98dad5c86d5fb657603064dc4
goyallucky <luckygoyal221982@gmail.com>
2025-06-13 08:20:47 +0530

    Initial Commit

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,1 +1,13 @@
 import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connection created successfully")
+    } catch(error){
+        console.log("Connection failed", error.message);
+        process.exit(1);
+    }
+}
+
+export default connectDB; 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import connectDB from './db/connection.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(3001,() =>{
+app.listen(process.env.PORT, () =>{
+    connectDB();
     console.log('Server is running on port 3001');
 })
