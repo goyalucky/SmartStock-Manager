96c5b9be3d02c2b18138c4ac7d2d49c5f5cb1ec9
goyallucky <luckygoyal221982@gmail.com>
2025-06-14 22:03:33 +0530

    Committed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Root from './utils/Root';
 import Login from './pages/Login'
 import ProtectedRoutes from './utils/ProtectedRoutes';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element= {<Root/>} />
-        <Route path="/admin/dashboard" element={<ProtectedRoutes requireRole={["admin"]}>
-          <h1>admin dashboard</h1>
-        </ProtectedRoutes>} />
+        <Route path="/admin-dashboard" element={<ProtectedRoutes requireRole={["admin"]}>
+          <Dashboard/>
+        </ProtectedRoutes>
+        } 
+        >
+          <Route index element={<h1>Summary of dashboard</h1>}/>
+          <Route path="categories" element={<h1>Categories</h1>}/>
+          <Route path="products" element={<h1>products</h1>}/>
+          <Route path="suppliers" element={<h1>suppliers</h1>}/>
+          <Route path="orders" element={<h1>Orders</h1>}/>
+          <Route path="users" element={<h1>Users</h1>}/>
+        </Route>
         <Route path="/customer/dashboard" element={<h1>customer dashboard</h1>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/unauthorized" element={<p className='font-bold text-3xl mt-20 ml-20'>Unauthorized</p>} />
