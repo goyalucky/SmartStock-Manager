bc4ae2821afd95301325afc174b5abe4f2a82fef
goyallucky <luckygoyal221982@gmail.com>
2025-06-27 23:39:04 +0530

    Product part added in it

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -4,8 +4,10 @@ import axios from 'axios';
 
 const Products = () => {
     const[openModal,setOpenModal] = useState(false);
+    const[editProduct,setEditProduct] = useState(null);
     const[categories,setCategories] = useState([]);
     const[suppliers,setSuppliers] = useState([]);
+    const [products,setProducts] = useState([]);
     const [formData,setFormData] = useState({
       name:'',
       description:'',
@@ -22,8 +24,14 @@ const Products = () => {
               Authorization: `Bearer ${localStorage.getItem('stock-token')}`,
             },
           });
-          setSuppliers(response.data.suppliers);
-          setCategories(response.data.categories);
+          if(response.data.success){
+            setSuppliers(response.data.suppliers);
+            setCategories(response.data.categories);
+            setProducts(response.data.products);
+          } else{
+            console.error("Error fetching products",response.data.message);
+            alert('Error fetching products. Please try again');
+          }
         } catch (error) {
           console.error('Error fetching suppliers',error);
         }
@@ -41,8 +49,66 @@ const Products = () => {
       }));
     }
 
+    const handleEdit = (product) => {
+      setOpenModal(true)
+      setEditProduct(product._id);
+      setFormData({
+        name:product.name,
+        description:product.description,
+        price:product.price,
+        stock:product.stock,
+        categoryId:product.categoryId._id,
+        supplierId:product.supplierId._id,
+      })
+    }
+
+    const closeModel = () => {
+      setOpenModal(false);
+      setEditProduct(null);
+      setFormData({
+        name:"",
+        description:"",
+        price:"",
+        stock:"",
+        categoryId:"",
+      })
+    }
+
      const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if(editProduct) {
+      try{
+        const response = await axios.put(
+          `http://localhost:3001/api/products/${editProduct}`,
+          formData,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("stock-token")}`,
+            },
+          }
+        );
+        if(response.data.success) {
+          alert("Product updated successfully");
+          setOpenModal(false);
+          setEditProduct(null);
+          setFormData({
+            name:"",
+            description:"",
+            price:"",
+            stock:"",
+            categoryId:"",
+            supplierId: "",
+            });
+        } else{
+          alert("Error updating product. Please try again");
+        }
+      } catch (error) {
+        alert("Error updating product. Please try again");
+      }
+      return;
+    }
+    else{
     try {
         const response = await axios.post(
           'http://localhost:3001/api/products/add',
@@ -56,7 +122,7 @@ const Products = () => {
         if (response.data.success) {
           // fetchSuppliers();
           alert('Product added successfully');
-          openModal(false)
+          setOpenModal(false)
           setFormData({
               name:"",
               description:"",
@@ -72,6 +138,7 @@ const Products = () => {
       // console.error('Error submitting supplier', error.message);
       alert('Error adding Product. Please try again');
     }
+  }
   };
 
   return (
@@ -91,13 +158,65 @@ const Products = () => {
           Add Product
         </button>
       </div>
+
+     <div>
+          <table className='w-full border-collapse border border-gray-300 mt-4'>
+            <thead>
+              <tr className='bg-gray-200'>
+                <th className='border border-gray-300 p-2'>S No</th>
+                <th className='border border-gray-300 p-2'>Product Name</th>
+                <th className='border border-gray-300 p-2'>Category Name</th>
+                <th className='border border-gray-300 p-2'>Supplier Name</th>
+                <th className='border border-gray-300 p-2'>Price</th>
+                <th className='border border-gray-300 p-2'>Stock</th>
+                <th className='border border-gray-300 p-2'>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => (
+                <tr key={product._id}>
+                  <td className='border border-gray-300 p-2'>{index + 1}</td>
+                  <td className='border border-gray-300 p-2'>{product.name}</td>
+                  <td className='border border-gray-300 p-2'>{product.categoryId.categoryName}</td>
+                  <td className='border border-gray-300 p-2'>{product.supplierId.name}</td>
+                  <td className='border border-gray-300 p-2'>{product.price}</td>
+                  <td className='border border-gray-300 p-2'>
+                    <span className='rounded-full font-semibold'>
+                      {product.stock == 0 ? (
+                        <span className='bg-red-100 text-red-500 px-2 py-1 rounded-full'>{product.stock}</span>
+                      ): product.stock < 5 ?(
+                          <span className='bg-yellow-100 text-yellow-600 px-2 py-1 rounded-full'>{product.stock}</span>
+                      ):(<span className='bg-green-100 text-green-500 px-2 py-1 rounded-full'>{product.stock}</span>)}
+                    </span>
+                  </td>
+                  <td className='border border-gray-300 p-2'>
+                    <button
+                      className='px-2 py-1 bg-yellow-500 text-white rounded mr-2'
+                      onClick={() => handleEdit(product)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className='px-2 py-1 bg-red-500 text-white rounded'
+                      onClick={() => handleDelete(product._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+         
+        </div>
+
       {openModal && (
         <div className='fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center z-50'>
           <div className='bg-white p-6 rounded shadow-md w-1/3 relative'>
             <h1 className='text-xl font-bold'>Add Product</h1>
             <button
               className='absolute top-4 right-4 font-bold text-lg cursor-pointer'
-              onClick={() => setOpenModal(false)}
+              onClick={closeModel}
             >
               X
             </button>
@@ -165,13 +284,13 @@ const Products = () => {
                   type="submit"
                   className='w-full mt-2 rounded bg-green-500 text-white p-3 hover:bg-green-600'
                 >
-                  Add Product
+                  {editProduct ? 'Save Changes' : 'Add Product'}
                 </button>
                 
                 <button
                   type='button'
-                  className='w-full mt-2 rounded bg-red-500 text-white p-3 hover:bg-red-600'
-                  onClick={() => setOpenModal(false)}
+                  className='w-full mt-2 rounded-md bg-red-500 text-white p-3 hover:bg-red-600'
+                  onClick={closeModel}
                 >
                   Cancel
                 </button>
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,10 +4,11 @@ import Product from "../models/Product.js";
 
 export const getProducts = async (req, res) => {
   try {
+    const products = await Product.find().populate('categoryId').populate('supplierId');
     const suppliers = await SupplierModal.find();
     const categories = await Category.find();
     console.log("Fetched suppliers: ", suppliers);
-    return res.status(200).json({ success: true, suppliers,categories });
+    return res.status(200).json({ success: true,products,suppliers,categories });
   } catch (error) {
     console.error("Error fetching suppliers:", error);
     return res.status(500).json({ success: false, message: "Server error in getting suppliers" });
@@ -27,3 +28,25 @@ export const addProduct = async (req, res) => {
     res.status(500).json({ success: false, message: "Server error" });
   }
 };
+
+export const updateProduct = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const { name, description, price, stock, categoryId, supplierId } = req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(id, { 
+      name, 
+      description,
+      price,
+      stock,
+      categoryId,
+      supplierId
+      }, { new: true });
+      if (!updatedProduct) {
+        return res.status(404).json({ success: false, message: "Product not found"});
+      }
+      res.status(200).json({ success: true, message: "Product updated successfully",product: updatedProduct });
+  } catch (error) {
+    console.error('Error updating Product:',error);
+    return res.status(500).json({success: false,message: 'Server error'});
+  }
+}
